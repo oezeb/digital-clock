ab1dc1d3f9cd20e4857ebb1de640aa24986681bf
oezeb <eouedraogo4@gmail.com>
2023-12-05 21:00:36 +0800

    edit clock to include edit time

--- test-clock.v
+++ test-clock.v
@@ -1,32 +1,41 @@
 module TestClock();
-    reg clk, set, reset;
+    reg clk, reset;
 
-    reg [5:0] sec_in;
-    reg [5:0] min_in;
-    reg [4:0] hour_in;
+    reg [1:0] select;
+    reg increment;
 
     wire [5:0] sec_out;
     wire [5:0] min_out;
     wire [4:0] hour_out;
 
+    integer i;
+
     initial begin
-        clk <= 0; set <= 0; reset <= 1;
-        sec_in <= 62; min_in <= 58; hour_in <= 1;
+        clk <= 0; reset <= 1;
+        select <= 0; increment <= 0;
         #1 reset <= 0;
-        #100 set <= 1;
-        #1 set <= 0;
-        #400 $finish;
+        #100 increment <= 1; #1 increment <= 0;
+        #1 select <= 1;
+        #1 increment <= 1; #1 increment <= 0;
+        #4 increment <= 1; #1 increment <= 0;
+        #1 select <= 2;
+        for (i = 0; i < 58; i = i + 1) begin
+            #1 increment <= 1; #1 increment <= 0;
+        end
+        #1 select <= 3;
+        for (i = 0; i < 23; i = i + 1) begin
+            #1 increment <= 1; #1 increment <= 0;
+        end
+        #100 $finish;
     end
 
     always #1 clk = ~clk;
 
     Clock Clock(
         .clk(clk), 
-        .set(set),
         .reset(reset),
-        .sec_in(sec_in),
-        .min_in(min_in),
-        .hour_in(hour_in),
+        .select(select),
+        .increment(increment),
         .sec_out(sec_out),
         .min_out(min_out),
         .hour_out(hour_out)
