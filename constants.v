a6a5e316cd1d56f170d0c6f08d87324a58f97dd2
oezeb <eouedraogo4@gmail.com>
2023-12-05 20:14:09 +0800

    add constants

---
+++ constants.v
@@ -0,0 +1,15 @@
+`ifndef CONSTANTS
+`define CONSTANTS
+
+// digital clock
+    `define MODE_CLOCK 0
+    `define MODE_STOPWATCH 1
+    `define MODE_ALARM_EDIT 2
+    `define MODE_CLOCK_EDIT 3
+
+// edit time
+    `define SELECT_NONE 0
+    `define SELECT_SEC 1
+    `define SELECT_MIN 2
+    `define SELECT_HOUR 3
+`endif
